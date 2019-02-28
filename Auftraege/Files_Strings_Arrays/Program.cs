8a42881d32676a71712b46740abec4bf87c334e3
CreepSore <creepsore@gmail.com>
2019-02-28 09:58:01 +0100

    Added Work #4

---
+++ Auftraege/Files_Strings_Arrays/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Files_Strings_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitConsole();
+
+            Console.Write(@"Please input the File that you want to check:\n > C:\Tmp\");
+            string filePath = @"C:\Tmp\" + Console.ReadLine();
+            if(!System.IO.File.Exists(filePath))
+            {
+                Console.WriteLine("Die Datei existiert nicht!");
+                Console.ReadKey(true);
+                System.Environment.Exit(0);
+            }
+
+            RunNormal(filePath);
+
+            Console.WriteLine("Press any key to exit ...");
+            Console.ReadKey(true);
+        }
+
+        static void RunNormal(string path)
+        {
+            Console.WriteLine("Anzahl der Zeilen: {0}", StringUtils.CountLines(path));
+            Console.WriteLine("Anzahl der Wörter: {0}", StringUtils.CountWords(path));
+            Console.WriteLine("Anzahl der Buchstaben: {0}", StringUtils.CountLetters(path));
+            int[] punctuationMarks = StringUtils.CountPunctuationMarks(path);
+            Console.WriteLine("Anzahl der Punkte: {0}", punctuationMarks[0]);
+            Console.WriteLine("Anzahl der Beistriche: {0}", punctuationMarks[1]);
+            Console.WriteLine();
+        }
+
+        static void InitConsole()
+        {
+            Console.SetWindowSize(80, 10);
+            Console.SetBufferSize(80, 10);
+            Console.SetWindowSize(80, 10);
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+        }
+    }
+}
---
+++ Auftraege/Files_Strings_Arrays/StringUtils.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Files_Strings_Arrays
+{
+    public static class StringUtils
+    {
+        /*
+         * Made Functions public so I can access it from Program.cs
+         */
+
+        public static int CountLines(string path)
+        {
+            if(!System.IO.File.Exists(path)) return -1;
+            string text = System.IO.File.ReadAllText(path, System.Text.Encoding.UTF8).Trim();
+           
+            return text.Split('\n').Length;
+        }
+
+        public static int CountWords(string path)
+        {
+            if (!System.IO.File.Exists(path)) return -1;
+            string text = System.IO.File.ReadAllText(path, System.Text.Encoding.UTF8);
+            text = text.Replace(',', ' ');
+            text = text.Replace('.', ' ');
+
+            while(text.Contains("  "))
+            {
+                text = text.Replace("  ", " ");
+            }
+
+            return text.Split(' ').Length;
+        }
+
+        public static int CountLetters(string path)
+        {
+            if (!System.IO.File.Exists(path)) return -1;
+            string text = System.IO.File.ReadAllText(path, System.Text.Encoding.UTF8).Trim();
+
+            int count = 0;
+            foreach (char c in text)
+            {
+                if ((char.IsLetter(c)))
+                    count++;
+            }
+
+            return count;
+        }
+
+        public static int[] CountPunctuationMarks(string path)
+        {
+            if (!System.IO.File.Exists(path)) return new int[] { -1, -1 };
+            string text = System.IO.File.ReadAllText(path, System.Text.Encoding.UTF8).Trim();
+
+            int[] count = { 0, 0 };
+            foreach (char c in text)
+            {
+                if(c == '.')
+                {
+                    count[0]++;
+                }
+                else if(c == ',')
+                {
+                    count[1]++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
