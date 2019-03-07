faecbddf0dea76e02406cc03b3674e87f94b729a
CreepSore <creepsore@gmail.com>
2019-03-07 09:30:12 +0100

    Finished "Flexible_Datenstrukturen"

---
+++ ITL/Auftraege/Flexible_Datenstrukturen/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+
+namespace Flexible_Datenstrukturen
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Run(@"C:\Tmp\students4.dat");
+            Console.ReadKey(true);
+
+        }
+
+        static void Run(string filepath)
+        {
+            string[] lines = System.IO.File.ReadAllLines(filepath);
+            foreach (string line in lines)
+            {
+                string name;
+                float avg;
+                AverageCalc(line, out name, out avg);
+                Console.WriteLine("{0} = {1}", name, avg);
+            }
+        }
+
+        static void AverageCalc(string line, out string name, out float average)
+        {
+            if (!line.Contains(':'))
+            {
+                name = null;
+                average = -1;
+                return;
+            }
+            average = 0;
+
+            name = line.Substring(0, line.IndexOf(':'));
+            string[] grades = line.Substring(line.IndexOf(':')).Split(',');
+            int correctGrades = 0;
+            for (int i = 0; i < grades.Length; i++)
+            {
+                string grade = grades[i].Trim();
+                if (!grade.Contains('='))
+                {
+                    continue;
+                }
+
+                string[] splittedGrade = grade.Split('=');
+                if (splittedGrade.Length < 2)
+                {
+                    continue;
+                }
+
+                float num = 0;
+                if (!float.TryParse(splittedGrade[1], out num))
+                {
+                    Console.WriteLine("Warning: Invalid Grade '{0}' at student '{1}' in class '{2}'", splittedGrade[1], name, splittedGrade[0]);
+                    correctGrades--;
+                }
+
+                correctGrades++;
+                average += num;
+            }
+
+            if (correctGrades != 0)
+            {
+                average /= correctGrades;
+            }
+            else
+            {
+                average = 0;
+            }
+            average = (float)Math.Round(average, 2);
+        }
+
+    }
+}
---
+++ ITL/Auftraege/Flexible_Datenstrukturen/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Flexible_Datenstrukturen")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Flexible_Datenstrukturen")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("c5a3c5c6-829a-4add-8591-3d5a68de26f7")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
