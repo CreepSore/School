b73e542f97c4cc44d6de5ca2ecf56c79c90d35be
CreepSore <creepsore@gmail.com>
2019-03-21 10:00:27 +0100

    Added new files

--- INF/OOPLib/MyLine.cs
+++ INF/OOPLib/MyLine.cs
@@ -6,61 +6,16 @@ using System.Threading.Tasks;
 
 namespace OOPLib
 {
-    public class MyLine : Object
+    public class MyLine : MyShape
     {
 
-        #region Members
-        private int x1, y1;
-        private int x2, y2;
-        #endregion
+        public MyLine(int x1, int y1) : base(x1, y1) { }
+        public MyLine(int x1, int y1, int x2, int y2) : base(x1, y1, x2, y2) { }
 
-        #region Properties
-        public int X1 { get => x1;
-            set {
-                if (value < 0)
-                    x1 = 0;
-                else if (value > 1000)
-                    x1 = 1000;
-                else
-                    x1 = value;
-            }
-        }
-        public int X2 { get => x2; set => x2 = value; }
-        public int Y1 { get => y1; set => y1 = value; }
-        public int Y2 { get => y2; set => y2 = value; }
-        #endregion
-
-        #region Constructor
-        public MyLine() { }
-
-        public MyLine(int x1, int y1)
+        public override void Draw()
         {
-            this.X1 = x1;
-            this.y1 = y1;
+            Console.WriteLine($"Line(XY)={X1},{Y1}; {X2},{Y2}");
         }
-
-        public MyLine(int x1, int y1, int x2, int y2) : this(x1, y1)
-        {
-            this.x2 = x2;
-            this.y2 = y2;
-        }
-        #endregion
-
-        #region Methods
-        public void Draw()
-        {
-            Console.WriteLine($"Linie(XY)={X1},{Y1}; {X2},{Y2}");
-        }
-
-        public void Move(int dx, int dy)
-        {
-            this.X1 += dx;
-            this.Y1 += dy;
-
-            this.X2 += dx;
-            this.Y2 += dy;
-        }
-        #endregion
 
     }
 }
